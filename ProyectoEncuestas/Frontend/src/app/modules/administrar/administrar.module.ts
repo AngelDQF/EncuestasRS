d52f3d6af554c7b6ac105a274dcd5522aef8927c
AngelDQF <angeldavidquintanilla@gmail.com>
2023-03-07 09:03:20 -0600

    creacion de modulos a implementar

---
+++ ProyectoEncuestas/Frontend/src/app/modules/administrar/administrar.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdministrarRoutingModule } from './administrar-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AdministrarRoutingModule
+  ]
+})
+export class AdministrarModule { }
