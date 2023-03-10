d4fc351979331af46fb19b952a7736ded859efcf
AngelDQF <angeldavidquintanilla@gmail.com>
2023-03-10 11:06:23 -0600

    arreglo en ruta de errores

--- ProyectoEncuestas/Frontend/src/app/modules/home/home-routing.module.ts
+++ ProyectoEncuestas/Frontend/src/app/modules/home/home-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from '@shared/components/error/error.component';
 import { IndexComponent } from '@shared/components/index/index.component';
 
 const routes: Routes = [
-  {path:'',component:IndexComponent},
-  {path:'administrar',loadChildren:()=>import('@modules/administrar/administrar.module').then(m=>m.AdministrarModule)},
-  {path:'usuarios',loadChildren:()=>import('@modules/usuarios/usuarios.module').then(m=>m.UsuariosModule)},
+  { path: '', component: IndexComponent },
+  { path: 'administrar', loadChildren: () => import('@modules/administrar/administrar.module').then(m => m.AdministrarModule) },
+  { path: 'usuarios', loadChildren: () => import('@modules/usuarios/usuarios.module').then(m => m.UsuariosModule) },
+  { path: '**', component: ErrorComponent }
 
 ];
 
