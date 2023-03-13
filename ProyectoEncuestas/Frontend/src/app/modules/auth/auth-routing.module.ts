9607ddba58b4f1ce0e4fc6a3bfec985b86490e64
AngelDQF <angeldavidquintanilla@gmail.com>
2023-03-13 10:07:33 -0600

    cambios para diseño responsive

--- ProyectoEncuestas/Frontend/src/app/modules/auth/auth-routing.module.ts
+++ ProyectoEncuestas/Frontend/src/app/modules/auth/auth-routing.module.ts
@@ -1,10 +1,16 @@
 import { LoginComponent } from './pages/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecuperarComponent } from './pages/recuperar/recuperar.component';
 
 const routes: Routes = [
-  {path:'login',component:LoginComponent},
-  {path:'**',redirectTo:'/auth/login'}
+  {
+    path: 'login', children: [
+      { path: '', component: LoginComponent },
+      { path: 'recuperar', component: RecuperarComponent }
+    ]
+  },
+  { path: '**', redirectTo: '/auth/login' }
 ];
 
 @NgModule({
--- ProyectoEncuestas/Frontend/src/app/modules/auth/auth.module.ts
+++ ProyectoEncuestas/Frontend/src/app/modules/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+import { RecuperarComponent } from './pages/recuperar/recuperar.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, RecuperarComponent,RecuperarComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
