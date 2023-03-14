44441d0f3a3984f1ba4e9a644da659bb5e5b4b22
AngelDQF <angeldavidquintanilla@gmail.com>
2023-03-14 15:41:51 -0600

    Creacion de HTML de Encuestas

--- ProyectoEncuestas/Frontend/src/app/modules/ecuestas/encuestas/encuestas.component.ts
+++ ProyectoEncuestas/Frontend/src/app/modules/ecuestas/encuestas/encuestas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Departamento, EncuestasService, UserDepartamentosResponse } from '@serv/encuestas.service';
 
@@ -7,7 +7,7 @@ import { Departamento, EncuestasService, UserDepartamentosResponse } from '@serv
   templateUrl: './encuestas.component.html',
   styleUrls: ['./encuestas.component.css', '../../../app.component.css']
 })
-export class EncuestasComponent {
+export class EncuestasComponent implements OnInit {
   title:string="Encuestas"
   formEncuestas:FormGroup=new FormGroup({});
   departamentos: Departamento[] = [];
@@ -16,4 +16,19 @@ export class EncuestasComponent {
       this.departamentos = data.results;
       })
   }
+  ngOnInit(): void {
+    this.formEncuestas=new FormGroup(
+      {
+        totalHombres:new FormControl(0,[
+          Validators.required,
+        ]),
+        totalMujeres:new FormControl(0,[
+          Validators.required,
+        ]),
+        totalAsistencia:new FormControl(0,[
+          Validators.required,
+        ])
+      }
+    )
+   }
 }
